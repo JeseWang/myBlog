56763fef44ac8b1335c620f6b866aba6960e4742
JeseWang <jessewang1024@gmail.com>
2017-02-22 17:48:46 +0800

    blog

--- hello.com/scripts/opening.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-	$('.opening').css({
-		'background':'#F75F5C',
-		'display': 'block'
-	}).fadeOut(3000)
-})
---
+++ myblog-1.0/routes/api.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var db = require('./db');
+
+router.get('/api/aticleList', function(req, res){
+
+})

6aaefcd8ea5760ef1e7cd2e10729dc63351629e3
王琦 <jessewang1024@gmail.com>
2019-05-07 23:30:56 +0800

    vuecli3.0

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="container">
+    <Header></Header>
+    <Nav id="nav">
+      <router-link to="/">首页</router-link> |
+      <router-link to="/article">文章</router-link> |
+      <router-link to="/about">关于我</router-link>
+    </Nav>
+    <!-- <router-view/> -->
+    <Footer></Footer>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Article.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{title}}页</p>
+</template>
+<script>
+export default {
+  name: 'article',
+  data () {
+    return {
+      title: '文章'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+p {
+  text-align: center;
+}
+</style>
